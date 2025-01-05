8d5bd5d68f08db1c4972fe72cf7639fc6ec42693
geekyneha <nehatualea@gmail.com>
2025-01-05 19:21:04 +0530

    Array

---
+++ 03-Arrays/index.js
@@ -0,0 +1,9 @@
+const strArr = ["apple", "banana", "cherry", "dragon fruit"];
+
+const numArr = [1, 2, 3, 4, 5, 6];
+
+const boolArr = [true, false];
+
+const mixed = ["a", 1, true, null, undefined, { a: "a" }, ["arr"]];
+
+console.log(mixed);
