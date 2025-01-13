bf27c9ae2028d0fa4be2c72326c36482f9dc7da6
geekyneha <nehatualea@gmail.com>
2025-01-13 19:18:09 +0530

    Armstrong Number

--- 03-Arrays/armStrongNum.js
+++ 03-Arrays/armStrongNum.js
@@ -1,2 +1,13 @@
-const armstrong = (num) => {};
-console.log(armstrong(123));
+const armstrong = (n) => {
+  let sum = 0;
+  let dup = n;
+  while (n > 0) {
+    let d = n % 10;
+
+    n = Math.floor(n / 10);
+    sum = sum + d * d * d;
+  }
+  if (dup == sum) return true;
+  else return false;
+};
+console.log(armstrong(371));
