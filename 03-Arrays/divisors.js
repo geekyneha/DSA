ed8d2feb4626d7c33a8f9cb0c29120a849a5fc30
geekyneha <nehatualea@gmail.com>
2025-01-13 19:43:52 +0530

    Print Divisors - optimized

--- 03-Arrays/divisors.js
+++ 03-Arrays/divisors.js
@@ -1,9 +1,15 @@
 const divsors = (num) => {
-  for (let i = 0; i <= num; i++) {
+  const arr = [];
+  for (let i = 0; i <= Math.sqrt(num); i++) {
     if (num % i == 0) {
-      console.log(i);
+      arr.push(i);
+      if (Math.floor(num / i) !== i) {
+        arr.push(Math.floor(num / i));
+      }
     }
   }
-  return;
+  return arr.sort(function (a, b) {
+    return a - b;
+  });
 };
-divsors(36);
+console.log(divsors(36));
