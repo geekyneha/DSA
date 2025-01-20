21e03a22e3f0884e7f0913eb0301df3b56440c11
geekyneha <nehatualea@gmail.com>
2025-01-20 22:01:25 +0530

    Container With Most Water

---
+++ 03-Arrays/containerMostWater.js
@@ -0,0 +1,24 @@
+var maxArea = function (height) {
+  let left = 0;
+  let right = height.length - 1;
+
+  let mostWater = 0;
+
+  while (left < right) {
+    let distance = right - left;
+    let minHeight = Math.min(height[left], height[right]);
+
+    let currHeight = minHeight * distance;
+
+    mostWater = Math.max(currHeight, mostWater);
+
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return mostWater;
+};
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
